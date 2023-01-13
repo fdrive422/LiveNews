f30a62a60b2344c6f87749caeee5363cd5869e65
fdrive422 <fafrago@gmail.com>
2023-01-12 21:08:23 -0800

    header title

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
 	return (
 		<>
-			<title>FDRIVE NEWS</title>
+			<title>FDRIVE News</title>
 			<meta
 				content="width=device-width, initial-scale=1"
 				name="viewport"
