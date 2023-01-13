6bf06c0f15e6540bc24040d9f4cb0f6f5567ee47
fdrive422 <fafrago@gmail.com>
2023-01-12 19:20:11 -0800

    firt commit

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "../styles/globals.css";
+import Header from "./Header";
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html>
+			<head />
+
+			<body>
+				<Header />
+				<div>{children}</div>
+			</body>
+		</html>
+	);
+}
