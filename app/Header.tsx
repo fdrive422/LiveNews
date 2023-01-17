9e17d7f8ef43dae5133899d882c3742adef7f8ed
fdrive422 <fafrago@gmail.com>
2023-01-16 23:14:37 -0800

    fix hydtraion error - moved provider tag inside body tag

--- app/Header.tsx
+++ app/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
 	return (
 		<header>
 			<div className="grid grid-cols-3 p-10 items-center">
-				<Bars3Icon className="h-8 w-8 cursor-pointer" />
+				<Bars3Icon className="h-8 w-8 cursor-pointer" href="/" />
 				<Link href="/" prefetch={false}>
 					<h1 className="font-serif text-4xl text-center">
 						The{" "}
--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,12 @@ export default function RootLayout({
 }) {
 	return (
 		<html>
-			<Providers>
-				<body className="bg-gray-100 dark:bg-zinc-900 transition-all duration-700">
+			<body className="bg-gray-100 dark:bg-zinc-900 transition-all duration-700">
+				<Providers>
 					<Header />
 					<div className="max-w-6xl mx-auto">{children}</div>
-				</body>
-			</Providers>
+				</Providers>
+			</body>
 		</html>
 	);
 }
