8ac05ce559f6ab06babf8c5f0b9f5f0a5fad3708
fdrive422 <fafrago@gmail.com>
2023-03-03 18:38:20 -0800

    head title change, subscribe button

--- app/Header.tsx
+++ app/Header.tsx
@@ -24,8 +24,8 @@ function Header() {
 				<div className="flex items-center justify-end space-x-2">
 					<DarkModeButton />
 
-					<button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">
-						Subscribe Now
+					<button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-3 rounded-full dark:bg-slate-800">
+						Subscribe
 					</button>
 				</div>
 			</div>
