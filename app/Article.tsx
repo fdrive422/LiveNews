08c85f0b33052c8b6bdb31d86fb4f6f8fc1564e4
fdrive422 <fafrago@gmail.com>
2023-07-07 19:58:32 -0700

    update tailwindcss properties

--- app/Article.tsx
+++ app/Article.tsx
@@ -2,44 +2,40 @@ import LiveTimestamp from "./LiveTimestamp";
 import ReadMoreButton from "./ReadMoreButton";
 
 type Props = {
-	article: Article;
+  article: Article;
 };
 
 function Article({ article }: Props) {
-	return (
-		<article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-lg hover:scale-105 hover:shadow-xl hover:bg-slate-200 transition-all duration-200 ease-out">
-			{article.image && (
-				<img
-					src={article.image}
-					alt={article.title}
-					className="h-56 w-full object-cover rounded-t-lg shadow-md"
-				/>
-			)}
+  return (
+    <article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-lg hover:scale-105 hover:shadow-xl hover:bg-slate-200 transition-all duration-200 ease-out">
+      {article.image && (
+        <img
+          src={article.image}
+          alt={article.title}
+          className="h-56 w-full object-cover rounded-t-lg shadow-md"
+        />
+      )}
 
-			<div className="flex-1 flex flex-col">
-				<div className="flex-1 flex flex-col p-5">
-					<h2 className="fond-bold font-serif line-clamp-2">
-						{article.title}
-					</h2>
+      <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col p-5">
+          <h2 className="fond-bold font-serif line-clamp-2">{article.title}</h2>
 
-					<section className="mt-2 flex-1">
-						<p className="text-xs line-clamp-3">
-							{article.description}
-						</p>
-					</section>
+          <section className="mt-2 flex-1">
+            <p className="text-xs line-clamp-3">{article.description}</p>
+          </section>
 
-					<footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-400">
-						<p>{article.source} -</p>
-						<p>
-							<LiveTimestamp time={article.published_at} />
-						</p>
-					</footer>
-				</div>
+          <footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-400">
+            <p>{article.source} -</p>
+            <p>
+              <LiveTimestamp time={article.published_at} />
+            </p>
+          </footer>
+        </div>
 
-				<ReadMoreButton article={article} />
-			</div>
-		</article>
-	);
+        <ReadMoreButton article={article} />
+      </div>
+    </article>
+  );
 }
 
 export default Article;
--- app/Header.tsx
+++ app/Header.tsx
@@ -5,38 +5,39 @@ import NavLinks from "./NavLinks";
 import SearchBox from "./SearchBox";
 
 function Header() {
-	return (
-		<header>
-			<div className="grid grid-cols-3 p-10 items-center">
-				<Bars3Icon className="h-8 w-8 cursor-pointer" href="/" />
-				<Link href="/" prefetch={false}>
-					<h1 className="font-serif text-4xl text-center">
-						The{" "}
-						<span className="underline decoration-6 decoration-orange-400">
-							Live
-						</span>{" "}
-						News
-					</h1>
-					{/* <p className="font-serif text-xsl text-center">
+  return (
+    <header>
+      <div className="grid grid-cols-3 p-10 items-center">
+        <Bars3Icon className="h-8 w-8 cursor-pointer" href="/" />
+        <Link href="/" prefetch={false}>
+          {/* text-[min(24vw,40px)] */}
+          <h1 className="font-serif text-4xl text-center">
+            The{" "}
+            <span className="underline decoration-6 decoration-orange-400">
+              Live
+            </span>{" "}
+            News
+          </h1>
+          {/* <p className="font-serif text-xsl text-center">
 						Brought to you by: FDrive
 					</p> */}
-				</Link>
-				<div className="flex items-center justify-end space-x-2">
-					<DarkModeButton />
+        </Link>
+        <div className="flex items-center justify-end space-x-2">
+          <DarkModeButton />
 
-					<button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-3 rounded-full dark:bg-slate-800">
-						Subscribe
-					</button>
-				</div>
-			</div>
+          <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-3 rounded-full dark:bg-slate-800">
+            Subscribe
+          </button>
+        </div>
+      </div>
 
-			{/* NavLinks */}
-			<NavLinks />
+      {/* NavLinks */}
+      <NavLinks />
 
-			{/* SearchBox */}
-			<SearchBox />
-		</header>
-	);
+      {/* SearchBox */}
+      <SearchBox />
+    </header>
+  );
 }
 
 export default Header;
